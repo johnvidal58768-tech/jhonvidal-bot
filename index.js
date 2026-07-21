edea98e00aab9cb4de32a68c0f773bbbbe5eefeb
johnvidal58768-tech <johnvidal.58768@gmail.com>
2026-07-21 02:46:13 -0500

    Update index.js

--- index.js
+++ index.js
@@ -2,5 +2,17 @@ const { Telegraf } = require('telegraf');
 
 const bot = new Telegraf('8837777955:AAFMcHE1hF4rO6lgnT1XEAT2h8JBmb79mq4');
 
-// নির্দিষ্ট চ্যাট আইডিতে মেসেজ পাঠানোর উদাহরণ
-bot.telegram.sendMessage('-1004330253784', 'হ্যালো! adbulmedia বট সচল রয়েছে।');
+// বটের মূল ফাংশন
+bot.start((ctx) => {
+    ctx.reply('স্বাগতম! adbulmedia টেলিগ্রাম বটে আপনাকে স্বাগতম।');
+});
+
+bot.on('text', (ctx) => {
+    const text = ctx.message.text;
+    // চাইলে নির্দিষ্ট চ্যাট আইডিতে মেসেজ পাঠাতে পারেন
+    bot.telegram.sendMessage('-1004330253784', `নতুন মেসেজ এসেছে: ${text}`);
+    ctx.reply('আপনার মেসেজটি গ্রহণ করা হয়েছে!');
+});
+
+bot.launch();
+console.log('Adbul Media Bot is running...');
