a0af8170f4d3ec506c0b080caeb1ebb5172a1842
johnvidal58768-tech <johnvidal.58768@gmail.com>
2026-07-21 02:49:38 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,14 +1,12 @@
 const { Telegraf } = require('telegraf');
 
 const bot = new Telegraf('8837777955:AAFMcHE1hF4rO6lgnT1XEAT2h8JBmb79mq4');
-const targetChatId = '-1004330253784'; // আপনার চ্যাট আইডি
+const targetChatId = '-1004330253784';
 
 bot.start((ctx) => {
-    // ইউজারের নাম এবং আইডি সংগ্রহ করা
     const username = ctx.from.username || ctx.from.first_name || 'Hunter_X_1';
     const userId = ctx.from.id;
     
-    // লিডের ডিজাইন করা মেসেজ
     const leadMessage = `🤝 **CONGRATULATIONS** 🤝\n\n` +
                         `🪪: \`${username}\` (${userId})\n` +
                         `🌎: United States\n` +
@@ -18,7 +16,6 @@ bot.start((ctx) => {
                         `🗼: Mobile\n\n` +
                         `📍 albany`;
 
-    // নির্দিষ্ট চ্যাটে লিড পাঠানো এবং Markdown ব্যবহার করা
     bot.telegram.sendMessage(targetChatId, leadMessage, { parse_mode: 'Markdown' })
         .then(() => {
             ctx.reply('আপনার লিড সফলভাবে সাবমিট হয়েছে!');
